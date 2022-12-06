75a98ec9be0fd209e1f876ee2abc7948f3eff039
vladildo <87080307+vladildo@users.noreply.github.com>
2022-12-06 17:51:29 +0000

    a1

--- src/Layouts/Page/AboutCompany/Components/Heading.js
+++ src/Layouts/Page/AboutCompany/Components/Heading.js
@@ -1,12 +1,12 @@
 import PrimaryButton from "../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../Components/SecondaryButton";
 function Heading() {
-    return (
-        <div className='d-flex flex-row justify-content-between px-1 mt-5'>
-            <h1 className='large-title'>Данные о компании</h1>
-            <SecondaryButton>Закрыть запись</SecondaryButton>
-        </div>
-    );
+  return (
+    <div className="d-flex flex-row justify-content-between px-1 m-5">
+      <h1 className="large-title">Данные о компании</h1>
+      <SecondaryButton>Закрыть запись</SecondaryButton>
+    </div>
+  );
 }
 
 export default Heading;
