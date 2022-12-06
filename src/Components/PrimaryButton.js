e0b782425600cb7c36d62660ff40e52611e52b8e
vladildo <87080307+vladildo@users.noreply.github.com>
2022-12-06 13:59:34 +0000

    Обновил стиль SecondaryButton

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 
 function PrimaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth; 
+    let minWidth = props.minWidth === undefined ? '217px' : props.minWidth; 
     const Button = styled.div`
         background: rgba(59, 130, 246, 1);
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
@@ -16,7 +16,7 @@ function PrimaryButton(props) {
             background: rgba(59, 130, 246, 0.8);
         }
         &:active {
-            background: rgba(15, 23, 42, 1);
+            background: rgba(15, 23, 42, 0.6);
         }
     `;
     return (
