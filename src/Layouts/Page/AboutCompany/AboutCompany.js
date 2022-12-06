9362312c2d45b94bbc2ea57141c9e28bdcf6a7a1
vladildo <87080307+vladildo@users.noreply.github.com>
2022-12-06 17:49:26 +0000

    a

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -2,18 +2,18 @@ import Heading from "./Components/Heading";
 import General from "./Components/General/General";
 import Addresses from "./Components/Addresses/Addresses";
 function AboutCompany() {
-    return (
-      <div>
-        <Heading /> {/* Заголовок и кнопка справа */}
-        <div className="d-flex justify-content-between gap-5 m-5">
-          <General /> {/* Основная информация вроде названия, типа и телефона */}
-          <div className="border-addres">
-            <Addresses />
-          </div>{" "}
-          {/* Список доступных адресов заведений */}
-        </div>
+  return (
+    <div>
+      <Heading /> {/* Заголовок и кнопка справа */}
+      <div className="d-flex justify-content-between gap-5 m-5">
+        <General /> {/* Основная информация вроде названия, типа и телефона */}
+        <div className="border-addres">
+          <Addresses />
+        </div>{" "}
+        {/* Список доступных адресов заведений */}
       </div>
-    );
-  }
+    </div>
+  );
+}
   
   export default AboutCompany;
