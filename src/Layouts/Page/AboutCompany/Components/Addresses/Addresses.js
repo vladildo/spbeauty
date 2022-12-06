efcf527519effbe06028481e7c8317075ca559ad
vladildo <87080307+vladildo@users.noreply.github.com>
2022-12-06 16:19:52 +0000

    Обновить Addresses

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,17 +1,20 @@
 import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
-            </div>
+      <div className='d-inline-flex flex-column gap-4 m-5 mb-2 py-2 px-1'>
+        <div className='title'>Адреса заведений</div>
+        <div className='d-inline-flex flex-column mb-5 gap-3 py-2'>
+          <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+          <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>{' '}
         </div>
+        <div className='d-inline-flex mt-5 gap-5 py-5 px-1'>
+          <SecondaryButton>Добавить филиал</SecondaryButton>
+        </div>
+      </div>
     );
-}
-
+  }
+  
 export default Addresses;
