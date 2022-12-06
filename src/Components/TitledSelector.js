bf8418be1ecf9be27679bd840af704f2051ed8c5
vladildo <87080307+vladildo@users.noreply.github.com>
2022-12-06 15:40:19 +0000

    Обновил TitledSelector

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 const Select = styled.select`
-    background: url("data:image/svg+xml,<svg height='10px' width='10px' viewBox='0 0 16 16' fill='%23000000' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg>") no-repeat;
+    background: rgba(248, 250, 252, 1);
     background-position: calc(100% - 0.75rem) center !important;
     -moz-appearance:none !important;
     -webkit-appearance: none !important; 
     appearance: none !important;
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
+    border-radius: 24px;
+    border: 1px solid #E2E8F0;
     background-color: #D9E2EF;
     color: #666787;
     &:hover {
@@ -19,7 +19,7 @@ const Select = styled.select`
 function TitledSelector({title, options}) {
     const Title = title === undefined ? 
         (<div></div>) : 
-        (<div className='caption rgba(148, 163, 184, 1)'>{title}</div>);
+        (<div className='caption'>{title}</div>);
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
