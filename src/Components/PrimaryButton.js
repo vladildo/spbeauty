30c0399b3160c7a2ae8cd79d455feedc0334c38d
vladildo <87080307+vladildo@users.noreply.github.com>
2022-12-06 14:01:46 +0000

    Убрал тень

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -8,7 +8,6 @@ function PrimaryButton(props) {
     let minWidth = props.minWidth === undefined ? '217px' : props.minWidth; 
     const Button = styled.div`
         background: rgba(59, 130, 246, 1);
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
         color: white;
         border-radius: 24px;
