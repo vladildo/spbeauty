3ed9c8cf2194cee5c84ba00cd3a61be98bd8acf9
vladildo <87080307+vladildo@users.noreply.github.com>
2022-12-06 15:42:26 +0000

    Обновить TitledSelector

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Select = styled.select`
-    background: rgba(248, 250, 252, 1);
+    background: url("data:image/svg+xml,<svg height='10px' width='10px' viewBox='0 0 16 16' fill='%23000000' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg>") no-repeat;
     background-position: calc(100% - 0.75rem) center !important;
     -moz-appearance:none !important;
     -webkit-appearance: none !important; 
@@ -9,7 +9,7 @@ const Select = styled.select`
     outline: none;
     border-radius: 24px;
     border: 1px solid #E2E8F0;
-    background-color: #D9E2EF;
+    background-color: rgba(248, 250, 252, 1);
     color: #666787;
     &:hover {
         background-color: #D9E2EFB8;
