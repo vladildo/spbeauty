09660b9ff200623d0843976fd8d6c40608a94b23
vladildo <87080307+vladildo@users.noreply.github.com>
2022-12-06 14:11:28 +0000

    Обновил стиль SerchField

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -9,13 +9,13 @@ const WrapperInput = styled.div`
 
 const Input = styled.input`
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
+    border-radius: 24px;
+    border: 1px solid #E2E8F0;
+    background-color: rgba(248, 250, 252, 1);
     color: #152842;
     width: 100%;
     &:hover {
-        background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+        background: rgba(248, 250, 252, 0.8);
     }
     &:focus {
         background: white;
