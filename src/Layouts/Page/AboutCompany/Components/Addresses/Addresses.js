3abf6f48aae4bd71e8fd27d48efdf459f2dd1dcd
vladildo <87080307+vladildo@users.noreply.github.com>
2022-12-07 18:12:15 +0000

    a

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,7 +4,7 @@ import ListElement from "./ListElement";
 
 function Addresses() {
     return (
-      <div className='d-flex flex-column justify-content-between p-4'>
+      <div className='d-flex flex-column'>
         <div className='d-flex flex-column gap-2'>
           <div className='title'>Адреса заведений</div>
           <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
